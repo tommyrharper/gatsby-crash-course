3b1f2089870f321a97808fa53bb53719f86f8eec
tommyrharper <tommyrharper@gmail.com>
2020-06-29 11:05:04 +0100

    Remove image and links on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,6 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Welcome to my website</h1>
     <p>This is a sample site for the Gatsby crash course</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
