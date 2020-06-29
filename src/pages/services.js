e709300763eba2a349c3268ccd6f1b7563f7c04b
tommyrharper <tommyrharper@gmail.com>
2020-06-29 11:19:48 +0100

    Add ServicesPage

---
+++ src/pages/services.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ServicesPage = () => {
+  return (
+    <div>
+      <h1>Our Services</h1>
+      <p>Some information about our services</p>
+    </div>
+  )
+}
+
+export default ServicesPage
