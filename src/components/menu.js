fe7ff82275697ea21d0f7147caa94266af7a2197
tommyrharper <tommyrharper@gmail.com>
2020-06-30 15:40:30 +0100

    Add links to menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "gatsby"
 
 const Menu = () => (
   <div style={{
@@ -11,10 +12,10 @@ const Menu = () => (
     display: 'flex',
     justifyContent: 'space-evenly'
   }}>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
+    <li><Link to="/">Home</Link></li>
+    <li><Link to="/">Home</Link></li>
+    <li><Link to="/">Home</Link></li>
+    <li><Link to="/">Home</Link></li>
   </ul>
 
   </div>
