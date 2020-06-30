3458874c0cd8dd404d984303fa8839d6ee5e4d20
tommyrharper <tommyrharper@gmail.com>
2020-06-30 15:41:14 +0100

    Correct link names and locations

--- src/components/menu.js
+++ src/components/menu.js
@@ -13,9 +13,8 @@ const Menu = () => (
     justifyContent: 'space-evenly'
   }}>
     <li><Link to="/">Home</Link></li>
-    <li><Link to="/">Home</Link></li>
-    <li><Link to="/">Home</Link></li>
-    <li><Link to="/">Home</Link></li>
+    <li><Link to="/about">About</Link></li>
+    <li><Link to="/services">Services</Link></li>
   </ul>
 
   </div>
