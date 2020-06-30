378824fec00a024d34e37a9df0a28d32ccdaafb6
tommyrharper <tommyrharper@gmail.com>
2020-06-30 15:19:15 +0100

    Simplify about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
-const AboutPage = () => {
-  return (
-    <div>
-      <h1>About Us</h1>
-      <p>This is some information about us</p>
-    </div>
-  )
-}
+const AboutPage = () => (
+  <Layout>
+    <SEO title="About" />
+    <h1>About Us</h1>
+    <p>This is some information about us</p>
+  </Layout>
+)
+
 
 export default AboutPage
