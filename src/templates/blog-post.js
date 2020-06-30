3cfcb49570b14a1a63dc1a5881697fc2b6bbd4fd
tommyrharper <tommyrharper@gmail.com>
2020-06-30 17:00:05 +0100

    Add Layout and SEO to blog-post.js

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import { Link } from "gatsby"
 
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
 export default function Template({data}) {
   const post = data.markdownRemark 
 
   return (
-    <div>
+    <Layout>
+      <SEO title={post.frontmatter.title} />
       <Link to="/blog">Go Back</Link>
       <hr/>
       <h1>{post.frontmatter.title}</h1>
       <h4>Posted by {post.frontmatter.author} on {post.frontmatter.date}</h4>
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
-    </div>
+    </Layout>
   )
 }
 
