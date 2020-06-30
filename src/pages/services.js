5527ea09a23a18aad458ef4e98f00413ebff470c
tommyrharper <tommyrharper@gmail.com>
2020-06-30 15:19:23 +0100

    Simplify services.js

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,12 +1,10 @@
 import React from 'react'
 
-const ServicesPage = () => {
-  return (
-    <div>
-      <h1>Our Services</h1>
-      <p>Some information about our services</p>
-    </div>
-  )
-}
+const ServicesPage = () =>(
+  <div>
+    <h1>Our Services</h1>
+    <p>Some information about our services</p>
+  </div>
+)
 
 export default ServicesPage
