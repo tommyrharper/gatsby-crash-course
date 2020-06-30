5c13544ad77c41a15b872d3eaed33f90b354d4a0
tommyrharper <tommyrharper@gmail.com>
2020-06-30 15:33:48 +0100

    Add list to menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,11 +1,24 @@
 import React from 'react'
 
-const Menu = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const Menu = () => (
+  <div style={{
+    background: '#f4f4f4',
+    paddingTop: '10px'
+  }}>
+
+  <ul style={{
+    listStyle: none,
+    display: 'flex',
+    justifyContent: 'space-evenly'
+  }}>
+    
+  </ul>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+  </div>
+)
+
 
 export default Menu;
